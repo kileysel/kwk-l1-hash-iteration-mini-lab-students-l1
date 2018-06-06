6c2b5d6c330f90f563ea542d82e368c5f18ee4e5
kileysel <kiley@selmanhouse.com>
2018-06-06 22:08:55 +0000

    Done.

---
+++ hashes.rb
@@ -0,0 +1,36 @@
+#hash = {"cheese" => "the stuff on pizza"} 
+
+# character_names = ["Monica ", "Joey", "Pheobe"] #key
+# characteristics = ["neat freak", "Jokester", "free spirit"] #value
+
+# friends_characters = {}
+
+# index = 0 
+# character_names.each do |name| 
+#   friends_characters[name] = characteristics[index]
+#   index += 1 
+# end 
+
+# puts friends_characters 
+
+celeb_crush={} #empty hash 
+celeb_crush["Emi"] = "Grant Gustin"
+
+puts celeb_crush #(this prints both key and value pair)
+
+class_celeb_crushes = {
+  "Natalia" => "Timothee Chalemet",
+  "Maya" => "Michael B. Jordan", 
+  "Amira" => "Cole Sprouse"
+}
+
+# puts class_celeb_crushes
+# puts class_celeb_crushes.values 
+# puts class_celeb_crushes.keys 
+
+#iterates through the hash to find keys/values 
+class_celeb_crushes.each do |names, crushes|
+ puts "#{names} crush is #{crushes}" 
+end 
+  
+  
